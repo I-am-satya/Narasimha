205dd2c959a55822b14b81aef93ba47f7745e6d2
Satya_venkat_kurella <satyavenkat.kurella@gmail.com>
2025-11-10 21:29:32 +0530

    Update generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -1,6 +1,6 @@
 from faker import Faker
 import numpy as np
-import pandas as pd
+import pandas as pd #comment added
 
 def generate_synthetic_data(num_samples=1000, seed=42):
   np.random.seed(seed)
